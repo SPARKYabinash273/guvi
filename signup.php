62cafa61f536518eca59753b36866311b68777d0
SPARKYabinash273 <64353169+SPARKYabinash273@users.noreply.github.com>
2020-12-31 15:28:19 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,10 +1,10 @@
 
 
 <?php
-$servername = "remotemysql.com";
-$username = "F0MyBrwA2Y";
-$password = "BzIuKsSyUi";
-$database = "F0MyBrwA2Y";
+$servername = "r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "xi405pg8ncdf3bs7";
+$password = "n07zrc7kikss3x78";
+$database = "tg8ggylf2hstpz6z";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
